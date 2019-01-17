b2fd2e256fb98038048ed1ccb123ba0cfb2d06e5
b0ho <ybg4828@gmail.com>
2019-01-17 16:49:31 +0900

    5

---
+++ index.ts
@@ -0,0 +1,8 @@
+
+const hi = (name:string, age:number, sex:string):string => {
+    return `my name is ${name} and i'm ${age} also i'm ${sex}`;
+}
+
+console.log(hi("name", 19, "male"));
+
+export {};
